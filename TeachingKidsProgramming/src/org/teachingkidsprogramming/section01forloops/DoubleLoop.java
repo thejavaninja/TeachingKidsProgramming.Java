831daf0d7c8b749d09b42436b0cc20a370e02888
thejavaninja <tannerhawkins2002@gmail.com>
2016-01-19 12:34:49 -0600

    DoubleLoopComplete

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/DoubleLoop.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/DoubleLoop.java
@@ -3,6 +3,7 @@ package org.teachingkidsprogramming.section01forloops;
 import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Grays;
 
 public class DoubleLoop
 {
@@ -10,34 +11,57 @@ public class DoubleLoop
   {
     Tortoise.show();
     //    Set the tortoise x position to 225 --#1.1
+    Tortoise.setX(225);
     //    Set the tortoise y position to 150 --#1.2
+    Tortoise.setY(150);
     //    UPDATE the tortoise speed to 10 --#8.2
-    Tortoise.setSpeed(5);
+    Tortoise.setSpeed(10);
     //  
     ColorWheel.addColor(PenColors.Reds.Crimson);
     ColorWheel.addColor(PenColors.Reds.DarkRed);
     ColorWheel.addColor(PenColors.Reds.FireBrick);
     //    Do the following 6 times --#3.1
-    //         Change the pen color of the line the tortoise draws to the next color on the Color Wheel --#5
-    //         Move the tortoise 4 times the current line number you are drawing --#4
-    //         Turn the tortoise 1/6 of 360 degrees to the left --#2
-    // 
-    //         Do the following 15 times --(HINT: The new line number is j) --#7.1
-    //              Set the pen width of the line the tortoise draws to 17 --#9
-    //              Move the tortoise 8 times the current line number you are drawing --#8.1
-    //              Turn the tortoise 1/5 of 360 degrees to the right --#6
-    //         Repeat --#7.2 
-    //  
-    //    Hide the Tortoise --#10
-    Tortoise.getBackgroundWindow().setBackground(PenColors.Yellows.PeachPuff);
-    //    Repeat --#3.2
+    for (int i = 0; i < 6; i++)
+    {
+      //         Change the pen color of the line the tortoise draws to the next color on the Color Wheel --#5
+      Tortoise.setPenColor(ColorWheel.getNextColor());
+      //         Move the tortoise 4 times the current line number you are drawing --#4
+      Tortoise.move(i * 4);
+      //         Turn the tortoise 1/6 of 360 degrees to the left --#2
+      Tortoise.turn(-60);
+      for (int j = 0; j < 15; j++)
+      {
+        // 
+        //         Do the following 15 times --(HINT: The new line number is j) --#7.1
+        //              Set the pen width of the line the tortoise draws to 17 --#9
+        Tortoise.setPenWidth(17);
+        //              Move the tortoise 8 times the current line number you are drawing --#8.1
+        Tortoise.move(j * 8);
+        //              Turn the tortoise 1/5 of 360 degrees to the right --#6
+        Tortoise.turn(0.2 * 360);
+      }
+      //         Repeat --#7.2 
+      //  
+      //    Hide the Tortoise --#10
+      Tortoise.hide();
+      Tortoise.getBackgroundWindow().setBackground(PenColors.Yellows.PeachPuff);
+      //    Repeat --#3.2
+    }
     //    Set the tortoise x position to 300 --#15.1
+    Tortoise.setX(300);
     //    Set the tortoise y position to 200 --#15.2
+    Tortoise.setY(200);
     //    Do the following 5 times --#12.1
-    //        Change the pen color of the line the tortoise draws to black --#14 
-    //        Move the Tortoise 25 pixels --#11
-    //        Turn the tortoise 1/5 of 360 degrees to the right --#13  
-    //    Repeat --#12.2
-    //  
+    for (int i = 0; i < 5; i++)
+    {
+      //        Change the pen color of the line the tortoise draws to black --#14 
+      Tortoise.setPenColor(Grays.Black);
+      //        Move the Tortoise 25 pixels --#11
+      Tortoise.move(25);
+      //        Turn the tortoise 1/5 of 360 degrees to the right --#13  
+      Tortoise.turn(.2 * 360);
+      //    Repeat --#12.2
+      //  
+    }
   }
 }
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquareQuiz.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquareQuiz.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.section01forloops;
 
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Yellows;
 import org.teachingkidsprogramming.recipes.quizzes.graders.SimpleSquareQuizGrader;
 import org.teachingkidsprogramming.recipes.quizzes.graders.SquareQuiz;
 
@@ -8,18 +10,22 @@ public class SimpleSquareQuiz implements SquareQuiz
   public void question1()
   {
     //  Move the tortoise 110 pixels
+    Tortoise.move(110);
   }
   public void question2()
   {
     //  Turn the tortoise 1/5 of 360 degrees to the right
+    Tortoise.turn(0.2 * 360);
   }
   public void question3()
   {
     //  Change the pen color the tortoise draws to yellow
+    Tortoise.setPenColor(Yellows.Yellow);
   }
   public void question4()
   {
     //  Change the width of the line the tortoise draws to 5 pixels
+    Tortoise.setPenWidth(5);
   }
   public static void main(String[] args)
   {
